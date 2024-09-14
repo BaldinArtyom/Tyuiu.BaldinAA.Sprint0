afeb151e24524c99ccab27f080dbbe84eb76bcb7
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-09-14 17:54:08 +0500

    Task1

---
+++ Tyuiu.BaldinAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.BaldinAA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Artyom";
+            int age = 19;
+            Console.WriteLine("Hello my name is " + name + " i am " + age + " years old!");
+            Console.ReadKey();
+        }
+    }
+}
