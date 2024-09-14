9afb4867c342921cece8c72beac5dca1eb422d3c
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-09-15 00:33:01 +0500

    Task3

---
+++ Tyuiu.BaldinAA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.BaldinAA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.BaldinAA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(8, 9));
+        }
+    }
+}
